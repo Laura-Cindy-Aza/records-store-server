23bd9dd74c832cfd7b5be7f1d2e83c10ac774168
limononoil <63930000+limononoil@users.noreply.github.com>
2021-03-16 15:10:34 +0100

    create CRUD controllers for users and records

---
+++ controllers/recordsControllers.js
@@ -0,0 +1,55 @@
+const Record = require('../models/Record');
+
+// GET /records => get all records
+exports.getRecords = async (req, res, next) => {
+    let allRecords = await Record.find();
+    res.send(allRecords);
+};
+
+// GET /records/:id => get single record by ID
+exports.getRecord = async (req, res, next) => {
+    const { id } = req.params;
+
+    try {
+        const record = await Record.findById(id);
+        res.json(record);
+    } catch (error) {
+        next(error)
+    };
+};
+
+// POST /records => create record
+exports.addRecord = async (req, res, next) => {
+    const { text } = req.body;
+
+    try {
+        const newRecord = await Record.create(req.body);
+        res.json(newRecord);
+    } catch (error) {
+        next(error)
+    };
+};
+
+// PATCH /records/:id => update record by ID
+exports.updateRecord = async (req, res, next) => {
+    const { id } = req.params;
+
+    try {
+        let updatedRecord = await Record.findByIdAndUpdate(id, req.body, { new: true });
+        res.json(updatedRecord);
+    } catch (err) {
+        next(err)
+    };
+};
+
+// DELETE /records/:id => remove record by ID
+exports.deleteRecord = async (req, res, next) => {
+    const { id } = req.params;
+
+    try {
+        let deletedRecord = await Record.findByIdAndDelete(id);
+        res.json(deletedRecord);
+    } catch (err) {
+        next(errorHandler(`A record with that ${id} doesn't exist`));
+    };
+};
