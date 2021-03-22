484f9825b72773f22cf6cbbf5210685506b7449e
Cindy <Punk.c@hotmail.com>
2021-03-22 10:49:46 +0100

    add recordscontrollers.js

--- controllers/recordsControllers.js
+++ controllers/recordsControllers.js
@@ -43,6 +43,51 @@ exports.updateRecord = async (req, res, next) => {
 };
 
 // DELETE /records/:id => remove record by ID
+const Record = require('../models/Record');
+
+// GET /records => get all records
+exports.getRecords = async (req, res, next) => {
+    let allRecords = await Record.find();
+    res.send(allRecords);
+};
+
+// GET /records/:id => get single record by ID
+exports.getRecord = async (req, res, next) => {
+    const { id } = req.params;
+
+    try {
+        const record = await Record.findById(id);
+        res.json(record);
+    } catch (error) {
+        next(error)
+    };
+};
+
+// POST /records => create record
+exports.addRecord = async (req, res, next) => {
+    const recordData = req.body;
+
+    try {
+        const newRecord = await Record.create(recordData);
+        res.json(newRecord);
+    } catch (error) {
+        next(error)
+    };
+};
+
+// PATCH /records/:id => update record by ID
+exports.updateRecord = async (req, res, next) => {
+    const { id } = req.params;
+
+    try {
+        let updatedRecord = await Record.findByIdAndUpdate(id, req.body, { new: true });
+        res.json(updatedRecord);
+    } catch (err) {
+        next(err)
+    };
+};
+
+// DELETE /records/:id => remove record by ID
 exports.deleteRecord = async (req, res, next) => {
     const { id } = req.params;
 
