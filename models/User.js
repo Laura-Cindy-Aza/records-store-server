52e2d06f2724256fdd879c033eae5b66e5d8bf76
Azael Alonso Campana <zarzarius@gmail.com>
2021-03-23 12:25:08 +0100

    login stuff added with Aghyad

--- models/User.js
+++ models/User.js
@@ -1,29 +1,30 @@
-const mongoose = require('mongoose');
-const {Schema, model} = mongoose;
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
 
 //UserSchema - contains rules about how every User should look like
-const UserSchema = new Schema ( {
-    firstName: { type: String, required: true, mix: 3, max: 50},
-    lastName: { type: String, required: true,mix: 3, max: 50,},
-    email: {type: String, unique:true, required: true, mix: 7, max: 50, unique: true},
-    nickName: { type: String, required: true,mix: 3, max: 50,unique:true},
-    password: {type: String, required:true, mix: 3, max: 50},
-    avatar: {type: String, required: true, default: '../assets/doge2.png'}
-},
-{
+const UserSchema = new Schema(
+  {
+    firstName: { type: String, required: true, mix: 3, max: 50 },
+    lastName: { type: String, required: true, mix: 3, max: 50 },
+    email: { type: String, unique: true, required: true, mix: 7, max: 50 },
+    nickName: { type: String, required: true, mix: 3, max: 50, unique: true },
+    password: { type: String, required: true, mix: 3, max: 50 },
+    avatar: { type: String, required: false, default: "../assets/doge2.png" },
+  },
+  {
     versionKey: false,
     timestamps: true,
-    toJSON:{
-        virtuals: true,
-    }
-}
-)
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
 
-UserSchema.virtual('fullName').get(function(){
-    return this.firstName + ' ' + this.lastName;
-})
+UserSchema.virtual("fullName").get(function () {
+  return this.firstName + " " + this.lastName;
+});
 
 //User model => out interface to db (=users collection )
-const User = model('User', UserSchema);
+const User = model("User", UserSchema);
 
 module.exports = User;
