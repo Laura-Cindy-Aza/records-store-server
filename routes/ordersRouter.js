c161e6fb8c5b30a30402fb23315a805bf2049108
Cindy <Punk.c@hotmail.com>
2021-04-12 13:17:42 +0200

    upload validation & sanitization

--- routes/ordersRouter.js
+++ routes/ordersRouter.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
+
 const {
     getOrder,
     addOrder,
     updateOrder,
     deleteOrder
 } = require("../controllers/ordersControllers");
+const { validateOrder } = require('../middleware/validation');
 
 // orders
-router.route("/").get(getOrders).post(addOrder);
+router.route("/").get(getOrder).post(validateOrder, addOrder);
 
 // orders/:id
-router.route("/:id").get(getOrder).patch(updateOrder);
+router.route("/:id").get(getOrder).patch(updateOrder).delete(deleteOrder);
 
 module.exports = router;
