c0c01d753ecd9d46f2d5c7ba6a381ba1b9b06dfa
Cindy <Punk.c@hotmail.com>
2021-04-13 13:48:00 +0200

    fix usersControllers (need debug) & add customError in helpers

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -3,6 +3,8 @@ const User = require("../models/User");
 const bcryptjs = require("bcryptjs");
 const cookieParser = require('cookie-parser');
 const jwt = require('jsonwebtoken');
+const customError = require('../helpers/customError');
+
 
 // GET /users => get all users
 exports.getUsers = async (req, res, next) => {
@@ -23,9 +25,16 @@ exports.getUser = async (req, res, next) => {
   }
 };
 
-// POST /users => create user
+// Signing up user => creating a new user from 0
 exports.addUser = async (req, res, next) => {
-  const { firstName, lastName, nickName, email, password } = req.body;
+  const userData = req.body;
+  try {
+
+  //take info in form given and create new user
+  const user = await User.create(userData)
+
+  // creating token
+  const token = user.generateAuthToken();
 
   // generating the salt
   const salt = await bcryptjs.genSalt(10);
@@ -33,25 +42,31 @@ exports.addUser = async (req, res, next) => {
   // hashing pass /// first argument is the text pass, the second is the salt
   const hashedPass = await bcryptjs.hash(password, salt);
 
-  try {
+    res.cookie('token', token,{
+      expires: new Date(Date.now() + 600000),
+      secure: false, //http
+      httpOnly: true
+    }).json(user);
+
     // const newUser = await User.create(userData);
-    const newUser = new User({
-      password: hashedPass,
-      firstName,
-      lastName,
-      email,
-      nickName,
-    });
+    // const newUser = new User({
+    //   password: hashedPass,
+    //   firstName,
+    //   lastName,
+    //   email,
+    //   nickName,
+    // });
     // user.avatar = `${req.protocoll}://${req.get("host")}${user.avatar}`;
-    const savedUser = await newUser.save();
-    res.send(savedUser);
+    //const savedUser = await newUser.save();
+    //res.send(savedUser);
   } catch (error) {
+    next(error);
     res.status(400).send(error);
   }
 };
 
-// CHECK USER
-exports.checkUser = async (req, res, next) => {
+// Log in user =>
+exports.loginUser = async (req, res, next) => {
   const { email, password } = req.body;
 
   try {
@@ -76,8 +91,9 @@ exports.updateUser = async (req, res, next) => {
 
   try {
     let updatedUser = await User.findByIdAndUpdate(id, req.body, { new: true });
-    user.avatar = `${req.protocoll}://${req.get("host")}${user.avatar}`;
-    res.json(updatedUser);
+    Object.assign(user, req.body)
+    const userUpdated = await updatedUser.save();
+    res.json(userUpdated);
   } catch (err) {
     next(err);
   }
@@ -89,8 +105,61 @@ exports.deleteUser = async (req, res, next) => {
 
   try {
     let deletedUser = await User.findByIdAndDelete(id);
+    if(!deletedUser) throw new Error();
     res.json(deletedUser);
   } catch (err) {
-    next(errorHandler(`A user with that ${id} doesn't exist`));
+    next(errorHandler(` user with ${id} doesn't exist`));
   }
+};
+
+//if user is wants to log out =>
+exports.logoutUser = async (req, res, next) => {
+  res.clearCookie('token')
+  res.json({ message: "Logged out " })
+};
+
+//if user wants to log in with email and password =>
+exports.loginUser = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    // JS find this user using email given pls 🍭
+    const userFound = await User.findOne({ email });
+
+    //what if email does not exist? then we send msg
+    if (!userFound) {
+      return next(
+        customError(
+          `No user with ${email}. Please try again`,
+          401
+        )
+      );
+    }
+
+    // compare the password given in plain text from frontend
+    // with the hashed password stored in database
+    const pwCompareResult = bcryptjs.compareSync(password, userFound.password);
+
+    if (!pwCompareResult) {
+      return next(customError('Wrong password', 401));
+    }
+
+    // Generate a token
+    const token = userFound.generateAuthToken();
+
+    // put the token in the response
+    res
+      .cookie('token', token, {
+        expires: new Date(Date.now() + 604800000),
+        secure: false, //http
+        httpOnly: true,
+      })
+      .json(userFound);
+  } catch (err) {
+    next(error);
+  }
+};
+
+exports.authUser = (req, res) => {
+  //req.user
+  res.json(req.user);
 };
