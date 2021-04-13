e08aeec894f904152bc43a38097cf6052caa0da0
Azael Alonso Campana <zarzarius@gmail.com>
2021-04-13 14:16:36 +0200

    typo in controllers fixed

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,20 +1,20 @@
-const customError = require('../helpers/customeError');
-const User = require('../models/User');
+const customError = require("../helpers/customError");
+const User = require("../models/User");
 
 exports.auth = async (req, res, next) => {
-    try {
-        // take the cookie/token from the request
-        const token = req.cookies.token;
-        // validate the cookie and check for the user with that _id
-        const user = await User.findByToken(token);
+  try {
+    // take the cookie/token from the request
+    const token = req.cookies.token;
+    // validate the cookie and check for the user with that _id
+    const user = await User.findByToken(token);
 
-        // if the token is corrupted, then throw an error
-        if (!user) next(customeError('User was not found'));
+    // if the token is corrupted, then throw an error
+    if (!user) next(customError("User was not found"));
 
-        // if token is valid
-        req.user = user;
-        next();
-    } catch (error) {
-        next(error);
-    }
+    // if token is valid
+    req.user = user;
+    next();
+  } catch (error) {
+    next(error);
+  }
 };
